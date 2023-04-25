f635a59ff00236d833d30c2d34f18d18f4c81f6b
basmaAyman <basmafulla@gmail.com>
2023-04-24 18:51:20 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
  * Description: 'print sentence'
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fprintf( stdout,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 			return (0);
 }
